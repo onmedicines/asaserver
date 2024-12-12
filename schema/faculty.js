4bff6db3989d1c0df91576abed4b3c86297f6972
onmedicines <semwalanurag26@gmail.com>
2024-12-12 10:50:51 +0530

    Update faculty schema to hold additional faculty info

--- schema/faculty.js
+++ schema/faculty.js
@@ -3,6 +3,13 @@ import mongoose from "mongoose";
 const facultySchema = mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  name: { type: String, required: true },
+  subjects: [
+    {
+      name: { type: String, required: true },
+      code: { type: String, required: true },
+    },
+  ],
 });
 
 export const Faculty = mongoose.model("Faculty", facultySchema);
